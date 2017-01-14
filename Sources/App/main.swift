e97b771e7f6ac2b4e30467fe36e07763b8f60aa3
Ben Scheirman <ben@scheirman.com>
2017-01-14 14:12:51 -0600

    Some refactoring, use locale fix for Linux

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,49 +3,55 @@ import Foundation
 
 let drop = Droplet()
 
+
+let exampleFormats = [
+  "EEEE, MMM d, yyyy",
+  "MM/dd/yyyy",
+  "MM-dd-yyyy HH:mm",
+  "MMM d, H:mm a",
+  "MMMM yyyy",
+  "MMM d, yyyy",
+  "E, d MMM yyyy HH:mm:ss Z",
+  "yyyy-MM-dd'T'HH:mm:ssZ",
+  "dd.MM.yy"
+]
+
+
 drop.get { req in
     let formatter = DateFormatter()
-    let localeIdentifier = "en_US_POSIX"
-    let locale = Locale(identifier: localeIdentifier)
+    let defaultLocaleIdentifier = "en_US_POSIX"
+    let locale = Locale(identifier: defaultLocaleIdentifier)
     formatter.locale = locale
     let date = Date()
-    let formats = [
-      "EEEE, MMM d, yyyy",
-      "MM/dd/yyyy",
-      "MM-dd-yyyy HH:mm",
-      "MMM d, H:mm a",
-      "MMMM yyyy",
-      "MMM d, yyyy",
-      "E, d MMM yyyy HH:mm:ss Z",
-      "yyyy-MM-dd'T'HH:mm:ssZ",
-      "dd.MM.yy"
-    ]
 
-    let examples: [Node] = formats.map { format in
-      formatter.dateFormat = format
-      let formatExample: [String: Node] = [
-        "format": Node.string(format),
-        "value": Node.string(formatter.string(from: date))
-      ]
-      return Node.object(formatExample)
+    let examples: [Node] = exampleFormats.map { format in
+        formatter.dateFormat = format
+        let formatExample: [String: Node] = [
+            "format": Node.string(format),
+            "value": Node.string(formatter.string(from: date))
+        ]
+        return Node.object(formatExample)
     }
 
     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
 
-    let locales: [Node] = Locale.availableIdentifiers.sorted().map {
-      // Stencil doesn't have Comparison operators so the only way is to check for
-      // the presence of `locale.default` on the front end.
-      var res = ["id" : Node.string($0)]
-      if $0 == localeIdentifier {
+    let locales = availableLocales().sorted()
+    let localeNodes: [Node] = locales.map { locale in
+
+      var res = ["id" : Node.string(locale)]
+
+      // set a default flag so the UI knows which select
+      if locale == defaultLocaleIdentifier {
           res["default"] = Node.string("localeIdentifier")
       }
+
       return Node.object(res)
     }
 
     let dateString = formatter.string(from: Date())
     let context = Node.object([
       "example_date": Node.string(dateString),
-      "locales": Node.array(locales),
+      "locales": Node.array(localeNodes),
       "formats": Node.array(examples)
     ])
 
@@ -53,15 +59,15 @@ drop.get { req in
 }
 
 drop.post("format") { req in
-    
+
     guard let format = req.data["format"]?.string,
         let dateString = req.data["date"]?.string?.removingPercentEncoding,
         let timeZoneOffset = req.data["time_zone_offset"]?.int
-    
+
     else {
       throw Abort.badRequest
     }
-    
+
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
     formatter.locale = Locale(identifier: req.data["locale"]?.string ?? "en_US_POSIX")
