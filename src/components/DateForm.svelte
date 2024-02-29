b2053ef24ccd5d14779eced39da25cb7684d1bdf
Ben Scheirman <ben@scheirman.com>
2024-02-29 17:33:02 -0600

    Fix overlay, sizing, compact width

--- src/components/DateForm.svelte
+++ src/components/DateForm.svelte
@@ -30,25 +30,33 @@
       result = response[0].value;
     });
   }
+
 </script>
 
+<!-- container -->
 <div class="sm:rounded-md shadow-xl my-4 p-4 sm:p-8 bg-white/10 backdrop-blur-md">
+
+  <!-- fields -->
   <div class="flex flex-col md:flex-row justify-between gap-8 md:gap-2">
-    <div class="flex flex-col flex-grow">
+
+    <!-- date field -->
+    <div class="flex flex-col flex-initial">
       <div class="text-sm font-medium uppercase text-gray-80">
         Date Input:
       </div>
       <input name="date-input" type="datetime-local" class="p-2 mt-1 h-10 mb-2 border-0 rounded-md bg-black/80" bind:value={inputDate}>
     </div>
 
-    <div class="flex flex-col flex-grow">
+    <!-- format field -->
+    <div class="flex flex-col grow">
       <div class="text-sm font-medium uppercase text-gray-80">
         Format text
       </div>
       <input type="text" name="format" class="p-2 mt-1 mb-2 h-10 border-0 rounded-md bg-black/80" bind:value={format}>
     </div>
 
-    <div class="flex flex-col flex-grow">
+    <!-- locale field -->
+    <div class="flex flex-col grow md:grow-0 md:w-48 z-50">
       <div class="text-sm font-medium uppercase text-gray-80">
         Locale
       </div>
@@ -56,6 +64,7 @@
       <LocaleSelector bind:locale locales={viewData.locales} />
     </div>
 
+    <!-- submit button -->
     <div class="flex items-end">
       <button on:click={sendRequest} name="format-btn" class="w-full format-btn h-10 shadow hover:shadow-none bg-white/25 hover:bg-white/20 backdrop-blur-lg rounded-md px-4 py-2 m-1 mb-2 transition">&rarr;</button>
     </div>
