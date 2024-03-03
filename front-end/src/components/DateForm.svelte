7c29c6ae165b1e13e1b2c159ef497cf863ccb530
Ben Scheirman <ben@scheirman.com>
2024-03-03 17:14:11 -0600

    Accessibility labels, keyboard form submission

--- front-end/src/components/DateForm.svelte
+++ front-end/src/components/DateForm.svelte
@@ -20,7 +20,6 @@
 
   function sendRequest() {
     let timestamp = new Date(inputDate).valueOf() / 1000;
-    console.log({ locale, timestamp, format })
     formatDate({
       locale,
       timestamp,
@@ -33,54 +32,56 @@
 
 </script>
 
-<!-- container -->
-<div class="sm:rounded-md shadow-xl my-4 p-4 sm:p-8 bg-white/10 backdrop-blur-md">
+<form on:submit|preventDefault={sendRequest}>
+  <!-- container -->
+  <div id="date-form-container" class="sm:rounded-md shadow-xl my-4 p-4 sm:p-8 bg-white/10 backdrop-blur-md">
 
-  <!-- fields -->
-  <div class="flex flex-col md:flex-row justify-between gap-8 md:gap-2">
+    <!-- fields -->
+    <div id="fields" class="flex flex-col md:flex-row justify-between gap-8 md:gap-2">
 
-    <!-- date field -->
-    <div class="flex flex-col flex-initial">
-      <div class="text-sm font-medium uppercase text-gray-80">
-        Date Input:
-      </div>
-      <input name="date-input" type="datetime-local" class="p-2 mt-1 h-10 mb-2 border-0 rounded-md bg-black/80" bind:value={inputDate}>
-    </div>
-
-    <!-- format field -->
-    <div class="flex flex-col grow">
-      <div class="text-sm font-medium uppercase text-gray-80">
-        Format text
-      </div>
-      <input type="text" name="format" class="p-2 mt-1 mb-2 h-10 border-0 rounded-md bg-black/80" bind:value={format}>
-    </div>
-
-    <!-- locale field -->
-    <div class="flex flex-col grow md:grow-0 md:w-48 z-50">
-      <div class="text-sm font-medium uppercase text-gray-80">
-        Locale
+      <!-- date field -->
+      <div class="flex flex-col flex-initial">
+        <label for="date-input" class="text-sm font-medium uppercase text-gray-80">
+          Date Input:
+        </label>
+        <input id="date-input" name="date-input" type="datetime-local" class="p-2 mt-1 h-10 mb-2 border-0 rounded-md bg-black/80" bind:value={inputDate}>
       </div>
 
-      <LocaleSelector bind:locale locales={viewData.locales} />
+        <!-- format field -->
+      <div class="flex flex-col grow">
+        <label for="format" class="text-sm font-medium uppercase text-gray-80">
+          Format text
+        </label>
+        <input id="format" type="text" name="format" class="p-2 mt-1 mb-2 h-10 border-0 rounded-md bg-black/80" bind:value={format}>
+      </div>
+
+      <!-- locale field -->
+      <div class="flex flex-col grow md:grow-0 md:w-48 z-50">
+        <label for="locale-selector" class="text-sm font-medium uppercase text-gray-80">
+          Locale
+        </label>
+
+        <LocaleSelector bind:locale locales={viewData.locales} />
+      </div>
+
+      <!-- submit button -->
+      <div class="flex items-end">
+        <button name="format-btn" class="w-full format-btn h-10 shadow hover:shadow-none bg-white/25 hover:bg-white/20 backdrop-blur-lg rounded-md px-4 py-2 m-1 mb-2 transition">&rarr;</button>
+      </div>
     </div>
 
-    <!-- submit button -->
-    <div class="flex items-end">
-      <button on:click={sendRequest} name="format-btn" class="w-full format-btn h-10 shadow hover:shadow-none bg-white/25 hover:bg-white/20 backdrop-blur-lg rounded-md px-4 py-2 m-1 mb-2 transition">&rarr;</button>
+    <div class="mt-10">
+      <div class="border-b border-b-white/20 text-white text-sm font-medium mb-4">
+        Result
+      </div>
+      <div class="text-4xl font-medium">
+        <span class="date-result">
+          {result}
+        </span>
+      </div>
     </div>
   </div>
-
-  <div class="mt-10">
-    <div class="border-b border-b-white/20 text-white text-sm font-medium mb-4">
-      Result
-    </div>
-    <div class="text-4xl font-medium">
-      <span class="date-result">
-        {result}
-      </span>
-    </div>
-  </div>
-</div>
+</form>
 
 <style>
   input {
