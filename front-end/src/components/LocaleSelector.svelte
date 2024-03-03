f5cf172e1695282338b8a0d882acc6cc2ab52fed
Ben Scheirman <ben@scheirman.com>
2024-03-03 17:14:29 -0600

    Better keyboard navigation for locale selector

--- front-end/src/components/LocaleSelector.svelte
+++ front-end/src/components/LocaleSelector.svelte
@@ -26,25 +26,31 @@
   */
   function handleNav(event) {
     let index = filteredLocales.indexOf(locale);
+    let newIndex = index;
     if (event.key == "ArrowUp") {
       if (index > 0) {
-        index = index - 1;
+        newIndex = index - 1;
       }
     } else if (event.key == "ArrowDown") {
       if (index < filteredLocales.length) {
-        index = index + 1;
+        newIndex = index + 1;
       }
+    } else if (event.key === "Enter" || event.key === " ") {
+      open = false
+      return
     } else {
       return
     }
 
     event.preventDefault();
 
-    // select new item and scroll into view
-    locale = filteredLocales[index];
-    let item = document.querySelector(`li[data-locale="${locale}"]`);
-    if (item) {
-      item.scrollIntoView({block: "nearest"});
+    if (newIndex != index) {
+      // select new item and scroll into view
+      locale = filteredLocales[newIndex];
+      let item = document.querySelector(`li[data-locale="${locale}"]`);
+      if (item) {
+        item.scrollIntoView({block: "nearest"});
+      }
     }
   }
 
@@ -56,14 +62,16 @@
     if (event.key == "Escape") {
       open = false;
     }
-
-    return handleNav(event);
   }
 
-  function onShow() {
-      console.log("on c u")
+    /** @param {KeyboardEvent} event
+     */
+  function handleKeyboard(event) {
+    console.log(event.key)
+    if (!open && (event.key == " " || event.key == "Enter")) {
+      open = true
+    }
   }
-
 
   onMount(() => {
     window.addEventListener("keydown", handleWindowKeypress);
@@ -73,15 +81,15 @@
   })
 </script>
 
-<Button class="p-2 mt-1 mb-2 h-10 border-0 rounded-md bg-black/80 hover:bg-black/60">
+<Button id="locale-selector" on:keydown={handleKeyboard} class="p-2 mt-1 mb-2 h-10 border-0 rounded-md bg-black/80 hover:bg-black/60">
   <span class="inline-block mr-2">{locale}</span>
   <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
     <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
   </svg>
 </Button>
-<Dropdown on:show={onShow} class="overflow-y-auto px-3 pb-3 text-sm h-60" containerClass="divide-y" bind:open>
+<Dropdown class="overflow-y-auto px-3 pb-3 text-sm h-60" containerClass="divide-y" bind:open>
   <div slot="header" class="p-3">
-    <Search size="sm" bind:value={term} on:keydown={handleNav} />
+    <Search size="sm" bind:value={term} autofocus on:keydown={handleNav} />
   </div>
   {#each filteredLocales as loc}
     <DropdownItem on:click={() => selectLocale(loc)} defaultClass="text-gray-500">
